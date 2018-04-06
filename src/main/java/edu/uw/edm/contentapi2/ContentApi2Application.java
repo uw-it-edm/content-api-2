fb3e0201bd27654b994ab2b423fe06c796e4e595
Maxime Deravet <maximed@uw.edu>
2018-04-06 11:37:55 -0700

    Add ApplicationPidFileWriter

--- src/main/java/edu/uw/edm/contentapi2/ContentApi2Application.java
+++ src/main/java/edu/uw/edm/contentapi2/ContentApi2Application.java
@@ -2,11 +2,15 @@ package edu.uw.edm.contentapi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ContentApi2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContentApi2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ContentApi2Application.class);
+        app.addListeners(new ApplicationPidFileWriter());
+
+        app.run(args);
+    }
 }
