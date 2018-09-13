23772e3d5f2026dd5fcf0a0bade41d52e61b0094
Andrew Gagne <agagne@uw.edu>
2018-09-13 11:59:41 -0700

    * SearchFilter should deserialize as SimpleSearchFilter

--- src/main/java/edu/uw/edm/contentapi2/controller/search/v1/model/query/SearchFilter.java
+++ src/main/java/edu/uw/edm/contentapi2/controller/search/v1/model/query/SearchFilter.java
@@ -1,4 +1,7 @@
 package edu.uw.edm.contentapi2.controller.search.v1.model.query;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = SimpleSearchFilter.class)
 public interface SearchFilter {
 }
