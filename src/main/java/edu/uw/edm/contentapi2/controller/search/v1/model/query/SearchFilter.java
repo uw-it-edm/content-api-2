30b8a343f429f9a4f1da4b0a61c93164d8a9568b
Andrew Gagne <agagne@uw.edu>
2018-09-13 13:01:56 -0700

    CAB-3202: Content-API-2 /search endpoint fails when specifying facet

--- src/main/java/edu/uw/edm/contentapi2/controller/search/v1/model/query/SearchFilter.java
+++ src/main/java/edu/uw/edm/contentapi2/controller/search/v1/model/query/SearchFilter.java
@@ -1,7 +1,4 @@
 package edu.uw.edm.contentapi2.controller.search.v1.model.query;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-@JsonDeserialize(as = SimpleSearchFilter.class)
 public interface SearchFilter {
 }
