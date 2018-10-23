5118203c3495a60fb7af44e4c91a64d246b1a3f0
Andrew Gagne <agagne@uw.edu>
2018-10-23 11:16:00 -0700

    CAB-3154: don't excape spaces in Range queries

--- src/main/java/edu/uw/edm/contentapi2/repository/acs/openapi/impl/SearchQueryBuilderImpl.java
+++ src/main/java/edu/uw/edm/contentapi2/repository/acs/openapi/impl/SearchQueryBuilderImpl.java
@@ -196,8 +196,10 @@ public class SearchQueryBuilderImpl implements SearchQueryBuilder {
         final String acsFieldName = getFieldNameForACSQuery(profile, simpleSearchFilter.getField(), user);
 
         final String searchFilterTerm = simpleSearchFilter.getTerm();
-        final String escapedSearchFilterTerm = searchFilterTerm.replace(" ","\\ ");
-        final String filterQuery = (simpleSearchFilter.isNot() ? NOT_TOKEN : EMPTY_STRING) + "(" + (isRangeQuery(escapedSearchFilterTerm) ? "" : TERM_EQUALS_TOKEN) + acsFieldName + SEARCH_IN_TERM_TOKEN + escapedSearchFilterTerm + ")";
+        final boolean isRangeQuery = isRangeQuery(searchFilterTerm);
+        final String escapedSearchFilterTerm = isRangeQuery? searchFilterTerm:searchFilterTerm.replace(" ","\\ ");
+
+        final String filterQuery = (simpleSearchFilter.isNot() ? NOT_TOKEN : EMPTY_STRING) + "(" + (isRangeQuery ? "" : TERM_EQUALS_TOKEN) + acsFieldName + SEARCH_IN_TERM_TOKEN + escapedSearchFilterTerm + ")";
         return filterQuery;
     }
 
