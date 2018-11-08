cf07f88b114379884fa376f0164865fde41c54e9
Maxime Deravet <maximede@users.noreply.github.com>
2018-11-08 10:18:21 -0800

    CAB-3231  - Implement publication endpoint (#85)
    * Implement publication endpoint
    
    * Add missing spring aop dependency needed for spring-retry
    
    * Fix code style issue
    
    * Add comment about deprecation status
    
    * Use camel case for test methods

---
+++ src/main/java/edu/uw/edm/contentapi2/controller/content/v1/ItemPublicationV1Controller.java
@@ -0,0 +1,51 @@
+package edu.uw.edm.contentapi2.controller.content.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import edu.uw.edm.contentapi2.controller.content.v1.model.PublicationResultResource;
+import edu.uw.edm.contentapi2.repository.exceptions.RepositoryException;
+import edu.uw.edm.contentapi2.security.User;
+import edu.uw.edm.contentapi2.service.PublicationService;
+
+/**
+ * @Deprecated : Once work-api is completely deprecated this endpoint will need to be removed
+ *
+ * @author Maxime Deravet Date: 11/2/18
+ */
+@RestController
+@Deprecated
+@RequestMapping("/content/v1/publication")
+public class ItemPublicationV1Controller {
+
+    private PublicationService publicationService;
+
+    @Autowired
+    public ItemPublicationV1Controller(PublicationService publicationService) {
+        this.publicationService = publicationService;
+    }
+
+    @Deprecated
+    @RequestMapping(
+            value = "{profile}/{clientProcessDefinitionKey},{clientProcessInstanceId}",
+            method = RequestMethod.POST)
+    public PublicationResultResource updatePublication(
+            @RequestParam Map<String, String> allRequestParams,
+            @PathVariable("profile") String profile,
+            @PathVariable("clientProcessDefinitionKey") String clientProcessDefinitionKey,
+            @PathVariable("clientProcessInstanceId") String clientProcessInstanceId,
+            @AuthenticationPrincipal User user) throws RepositoryException {
+
+        PublicationResultResource publicationResultResource = publicationService.updatePublication(allRequestParams, profile, clientProcessDefinitionKey, clientProcessInstanceId, user);
+
+        return publicationResultResource;
+
+    }
+}
